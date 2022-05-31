36aa6094d6e65d39ad9753d31df08e55f7be3829
Alice Seaborn <seaborn.dev@gmail.com>
2022-05-30 23:04:57 -0600

    [ENH] Render delete button red

--- project/source/forms/DeleteBookButton.py
+++ project/source/forms/DeleteBookButton.py
@@ -4,5 +4,7 @@ from flask_wtf import FlaskForm
 from wtforms.fields import SubmitField
 
 
+delete_style={'type': 'button', 'class': 'btn btn-danger'}
+
 class DeleteBookButton(FlaskForm):
-	delete = SubmitField('Delete')
+	delete = SubmitField('Delete', render_kw=delete_style)
