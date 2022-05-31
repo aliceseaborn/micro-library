697ab503a850997f9399e7d873ab1b726a019762
Alice Seaborn <seaborn.dev@gmail.com>
2022-05-30 20:34:32 -0600

    [FIX] Adjust Book fieldtypes

--- project/source/models/Book.py
+++ project/source/models/Book.py
@@ -15,21 +15,22 @@ class Book(db.Model):
 	subject = db.Column(db.String(16))
 	synopsis = db.Column(db.Text)
 	fiction = db.Column(db.Boolean)
-	year = db.Column(db.DateTime)
+	year = db.Column(db.String(4))
 	available = db.Column(db.Boolean)
 	isbn = db.Column(db.String(16), unique=True)
 	tags = db.Column(db.Text)
 	
-	def __init__(self, data):
-		self.title = data["title"]
-		self.author = data["author"]
-		self.fiction = self._process_boolean(data["fiction"])
-		self.year = self._process_year(data["year"])
-		self.subject = data["subject"]
-		self.synopsis = data["synopsis"]
-		self.isbn = data["isbn"]
-		self.available = self._process_boolean(data["available"])
-		self.tags = data["tags"]
+	def __init__(self, title, author, fiction, year,
+		subject, synopsis, isbn, available, tags):
+		self.title = title
+		self.author = author
+		self.fiction = self._process_boolean(fiction)
+		self.year = year
+		self.subject = subject
+		self.synopsis = synopsis
+		self.isbn = isbn
+		self.available = self._process_boolean(available)
+		self.tags = tags
 		
 	def __repr__(self):
 		return f"<Book {self.isbn} by {self.author}.>"
@@ -37,7 +38,4 @@ class Book(db.Model):
 	def _process_boolean(self, data):
 		if data == "True": return True
 		else: return False
-
-	def _process_year(self, data):
-		return datetime(data, 1, 1)
 
